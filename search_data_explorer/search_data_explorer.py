fd2bda89f12cc18fa7eaa84c3e5bf893a59f9ea0
Simon Blanke <simon.blanke@yahoo.com>
2022-10-30 11:29:15 +0100

    add port argument

--- search_data_explorer/search_data_explorer.py
+++ search_data_explorer/search_data_explorer.py
@@ -11,32 +11,39 @@ class SearchDataExplorer:
         self.plots = plots
         self.parameters = parameters
 
-    def _run_streamlit(self, path):
+    def _run_streamlit(self, path, port):
+        if port == "auto":
+            port_cmd = ""
+        else:
+            port_cmd = " --server.port " + str(port)
+
         abspath = os.path.abspath(__file__)
         dname = os.path.dirname(abspath)
 
         arg1 = path + " "
         args = " ".join(self.plots)
 
-        command = "streamlit run " + dname + "/streamlit_run.py " + arg1 + args
+        command = (
+            "streamlit run " + dname + "/streamlit_run.py " + arg1 + args + port_cmd
+        )
         os.system(command)
 
-    def open(self, input=None):
+    def open(self, input=None, port="auto"):
         if isinstance(input, pd.DataFrame):
             df = input
             path_tmp = "./df_temp.csv~"
 
             df.to_csv(path_tmp, index=False)
 
-            self._run_streamlit(path_tmp)
+            self._run_streamlit(path_tmp, port)
 
             if os.path.exists(path_tmp):
                 os.remove(path_tmp)
 
         elif isinstance(input, str):
             path = input
-            self._run_streamlit(path)
+            self._run_streamlit(path, port)
 
         elif input is None:
             path = "no_path"
-            self._run_streamlit(path)
+            self._run_streamlit(path, port)
