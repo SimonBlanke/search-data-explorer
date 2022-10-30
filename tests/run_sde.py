e2aaddeb1b0976528f42a67a74f025e73d0a5f3f
Simon Blanke <simon.blanke@yahoo.com>
2022-10-30 11:29:47 +0100

    add basic test files

---
+++ tests/run_sde.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+from search_data_explorer import SearchDataExplorer
+
+
+here = os.path.dirname(os.path.realpath(__file__))
+path2file = os.path.join(here, "test_data.csv")
+
+
+port = sys.argv[-1]
+print("Connect to port:", port)
+
+board = SearchDataExplorer()
+board.open(path2file, port=port)
