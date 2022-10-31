8a2462adbdaf2403ff910ad7abc62aa787945bb8
Simon Blanke <simon.blanke@yahoo.com>
2022-10-31 20:41:11 +0100

    run streamlit script in background

--- tests/run_sde.py
+++ tests/run_sde.py
@@ -7,9 +7,9 @@ from search_data_explorer import SearchDataExplorer
 here = os.path.dirname(os.path.realpath(__file__))
 path2file = os.path.join(here, "test_data.csv")
 
-
+"""
 port = sys.argv[-1]
 print("Connect to port:", port)
-
+"""
 board = SearchDataExplorer()
-board.open(path2file, port=port)
+board.open(path2file)
